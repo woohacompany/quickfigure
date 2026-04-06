10cc410b0661ea2bea7eaa457d1638d33da88c9f
woose <woohacompany@gmail.com>
2026-04-06 18:03:18 +0900

    feat: add Google Analytics 4 tracking

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
@@ -89,6 +90,13 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
+        <Script src="https://www.googletagmanager.com/gtag/js?id=G-EVBRPZ64BJ" strategy="afterInteractive" />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-EVBRPZ64BJ');`}
+        </Script>
       </body>
     </html>
   );
